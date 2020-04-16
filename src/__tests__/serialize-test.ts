293ffb86272f7574772b2c774f9856017c8c09a0
Peter Beshai <pbeshai@netflix.com>
2020-04-16 11:46:10 -0700

    Update serialization for new handling of null and empty #19

--- src/__tests__/serialize-test.ts
+++ src/__tests__/serialize-test.ts
@@ -32,9 +32,8 @@ describe('serialize', () => {
     });
 
     it('handles null and undefined', () => {
-      const result = encodeDate(null);
-      expect(result).toBeUndefined();
-      expect(encodeDate(undefined)).not.toBeDefined();
+      expect(encodeDate(null)).toBeNull();
+      expect(encodeDate(undefined)).toBeUndefined();
     });
   });
 
@@ -54,15 +53,14 @@ describe('serialize', () => {
     });
 
     it('handles null and undefined', () => {
-      const result = decodeDate(null);
-      expect(result).not.toBeDefined();
-      expect(decodeDate(undefined)).not.toBeDefined();
-      expect(decodeDate('')).not.toBeDefined();
+      expect(decodeDate(null)).toBeNull();
+      expect(decodeDate('')).toBeNull();
+      expect(decodeDate(undefined)).toBeUndefined();
     });
 
     it('handles malformed input', () => {
       const result = decodeDate('foo-one-two');
-      expect(result).not.toBeDefined();
+      expect(result).toBe(null);
     });
 
     it('handles array of values', () => {
@@ -77,7 +75,8 @@ describe('serialize', () => {
     it('produces the correct value', () => {
       expect(encodeBoolean(true)).toBe('1');
       expect(encodeBoolean(false)).toBe('0');
-      expect(encodeBoolean(undefined)).not.toBeDefined();
+      expect(encodeBoolean(undefined)).toBeUndefined();
+      expect(encodeBoolean(null)).toBeNull();
     });
   });
 
@@ -85,12 +84,13 @@ describe('serialize', () => {
     it('produces the correct value', () => {
       expect(decodeBoolean('1')).toBe(true);
       expect(decodeBoolean('0')).toBe(false);
-      expect(decodeBoolean(undefined)).not.toBeDefined();
-      expect(decodeBoolean('')).not.toBeDefined();
+      expect(decodeBoolean(undefined)).toBeUndefined();
+      expect(decodeBoolean('')).toBeNull();
+      expect(decodeBoolean(null)).toBeNull();
     });
 
     it('handles malformed input', () => {
-      expect(decodeBoolean('foo')).not.toBeDefined();
+      expect(decodeBoolean('foo')).toBeNull();
     });
 
     it('handles array of values', () => {
@@ -102,7 +102,8 @@ describe('serialize', () => {
     it('produces the correct value', () => {
       expect(encodeNumber(123)).toBe('123');
       expect(encodeNumber(-32.12)).toBe('-32.12');
-      expect(encodeNumber(undefined)).not.toBeDefined();
+      expect(encodeNumber(undefined)).toBeUndefined();
+      expect(encodeNumber(null)).toBeNull();
     });
   });
 
@@ -110,12 +111,13 @@ describe('serialize', () => {
     it('produces the correct value', () => {
       expect(decodeNumber('99')).toBe(99);
       expect(decodeNumber('-58.21')).toBe(-58.21);
-      expect(decodeNumber(undefined)).not.toBeDefined();
-      expect(decodeNumber('')).not.toBeDefined();
+      expect(decodeNumber(undefined)).toBeUndefined();
+      expect(decodeNumber('')).toBeNull();
+      expect(decodeNumber(null)).toBeNull();
     });
 
     it('handles malformed input', () => {
-      expect(decodeNumber('foo')).not.toBeDefined();
+      expect(decodeNumber('foo')).toBeNaN();
     });
 
     it('handles array of values', () => {
@@ -126,7 +128,9 @@ describe('serialize', () => {
   describe('encodeString', () => {
     it('produces the correct value', () => {
       expect(encodeString('foo')).toBe('foo');
-      expect(encodeString(undefined)).not.toBeDefined();
+      expect(encodeString('')).toBe('');
+      expect(encodeString(null)).toBeNull();
+      expect(encodeString(undefined)).toBeUndefined();
     });
   });
 
@@ -134,21 +138,20 @@ describe('serialize', () => {
     it('produces the correct value', () => {
       expect(decodeString('bar')).toBe('bar');
       expect(decodeString('')).toBe('');
-      expect(decodeString(undefined)).not.toBeDefined();
-      expect(decodeString(null)).not.toBeDefined();
+      expect(decodeString(undefined)).toBeUndefined();
+      expect(decodeString(null)).toBeNull();
     });
 
     it('handles array of values', () => {
       expect(decodeString(['foo', 'bar'])).toBe('foo');
     });
   });
-
   describe('encodeJson', () => {
     it('produces the correct value', () => {
       const input = { test: '123', foo: [1, 2, 3] };
       expect(encodeJson(input)).toBe(JSON.stringify(input));
-      expect(encodeJson(undefined)).not.toBeDefined();
-      expect(encodeJson(null)).not.toBeDefined();
+      expect(encodeJson(undefined)).toBeUndefined();
+      expect(encodeJson(null)).toBeNull();
       expect(encodeJson(0)).toBe('0');
       expect(encodeJson(false)).toBe('false');
     });
@@ -162,12 +165,13 @@ describe('serialize', () => {
         jim: ['grill'],
       };
       expect(output).toEqual(expectedOutput);
-      expect(decodeJson(undefined)).not.toBeDefined();
-      expect(decodeJson('')).not.toBeDefined();
+      expect(decodeJson(undefined)).toBeUndefined();
+      expect(decodeJson(null)).toBeNull();
+      expect(decodeJson('')).toBeNull();
     });
 
     it('handles malformed input', () => {
-      expect(decodeJson('foo')).not.toBeDefined();
+      expect(decodeJson('foo')).toBeNull();
     });
 
     it('handles array of values', () => {
@@ -179,7 +183,10 @@ describe('serialize', () => {
     it('produces the correct value', () => {
       const input = ['a', 'b', 'c'];
       expect(encodeArray(input)).toEqual(['a', 'b', 'c']);
-      expect(encodeArray(undefined)).not.toBeDefined();
+      expect(encodeArray(undefined)).toBeUndefined();
+      expect(encodeArray(null)).toBeNull();
+      expect(encodeArray([])).toEqual([]);
+      expect(encodeArray(['a', null, '', 'b'])).toEqual(['a', null, '', 'b']);
     });
   });
 
@@ -189,12 +196,14 @@ describe('serialize', () => {
       const expectedOutput = ['a', 'b', 'c'];
 
       expect(output).toEqual(expectedOutput);
-      expect(decodeArray(undefined)).not.toBeDefined();
-      expect(decodeArray('')).not.toBeDefined();
+      expect(decodeArray(undefined)).toBeUndefined();
+      expect(decodeArray('')).toEqual([]);
+      expect(decodeArray([])).toEqual([]);
+      expect(decodeArray(null)).toBeNull();
     });
 
-    it('filters empty values', () => {
-      expect(decodeArray(['a', '', 'b'])).toEqual(['a', 'b']);
+    it('handles empty values', () => {
+      expect(decodeArray(['a', '', null, 'b'])).toEqual(['a', '', null, 'b']);
     });
   });
 
@@ -202,7 +211,9 @@ describe('serialize', () => {
     it('produces the correct value', () => {
       const input = [1, 2, 3];
       expect(encodeNumericArray(input)).toEqual(['1', '2', '3']);
-      expect(encodeNumericArray(undefined)).not.toBeDefined();
+      expect(encodeNumericArray(undefined)).toBeUndefined();
+      expect(encodeNumericArray(null)).toBeNull();
+      expect(encodeNumericArray([])).toEqual([]);
     });
   });
 
@@ -212,38 +223,111 @@ describe('serialize', () => {
       const expectedOutput = [1, 2, 3];
 
       expect(output).toEqual(expectedOutput);
-      expect(decodeNumericArray(undefined)).not.toBeDefined();
-      expect(decodeNumericArray('')).not.toBeDefined();
+      expect(decodeNumericArray(undefined)).toBeUndefined();
+      expect(decodeNumericArray(null)).toBeNull();
+      expect(decodeNumericArray('')).toEqual([]);
+      expect(decodeNumericArray([])).toEqual([]);
     });
 
-    it('filters empty and NaN values', () => {
-      expect(decodeNumericArray(['1', '', '2', 'foo', '3'])).toEqual([1, 2, 3]);
+    it('handles empty and NaN values', () => {
+      expect(decodeNumericArray(['1', '', '2', 'foo', '3'])).toEqual([
+        1,
+        null,
+        2,
+        NaN,
+        3,
+      ]);
+    });
+  });
+
+  describe('encodeDelimitedArray', () => {
+    it('produces the correct value', () => {
+      const input = ['a', 'b', 'c'];
+      expect(encodeDelimitedArray(input)).toBe('a_b_c');
+      expect(encodeDelimitedArray(undefined)).toBeUndefined();
+      expect(encodeDelimitedArray(null)).toBeNull();
+      expect(encodeDelimitedArray([])).toBe('');
+    });
+  });
+
+  describe('decodeDelimitedArray', () => {
+    it('produces the correct value', () => {
+      const output = decodeDelimitedArray('a_b_c');
+      const expectedOutput = ['a', 'b', 'c'];
+
+      expect(output).toEqual(expectedOutput);
+      expect(decodeDelimitedArray(undefined)).toBeUndefined();
+      expect(decodeDelimitedArray('')).toEqual([]);
+      expect(decodeDelimitedArray(null)).toBe(null);
+    });
+
+    it('handles empty values', () => {
+      expect(decodeDelimitedArray('__')).toEqual(['', '', '']);
+    });
+
+    it('handles array of values', () => {
+      expect(decodeDelimitedArray(['foo_bar', 'jim_grill'])).toEqual([
+        'foo',
+        'bar',
+      ]);
+    });
+  });
+
+  describe('encodeDelimitedNumericArray', () => {
+    it('produces the correct value', () => {
+      const input = [9, 4, 0];
+      expect(encodeDelimitedNumericArray(input)).toBe('9_4_0');
+      expect(encodeDelimitedNumericArray(undefined)).toBeUndefined();
+      expect(encodeDelimitedNumericArray(null)).toBe(null);
+      expect(encodeDelimitedNumericArray([])).toBe('');
+    });
+  });
+
+  describe('decodeDelimitedNumericArray', () => {
+    it('produces the correct value', () => {
+      const output = decodeDelimitedNumericArray('9_4_0');
+      const expectedOutput = [9, 4, 0];
+
+      expect(output).toEqual(expectedOutput);
+      expect(decodeDelimitedNumericArray(undefined)).toBeUndefined();
+      expect(decodeDelimitedNumericArray('')).toEqual([]);
+      expect(decodeDelimitedNumericArray(null)).toBeNull();
+    });
+
+    it('filters empty values', () => {
+      expect(decodeDelimitedNumericArray('__')).toEqual([null, null, null]);
+    });
+
+    it('handles array of values', () => {
+      expect(decodeDelimitedNumericArray(['1_2', '3_4'])).toEqual([1, 2]);
     });
   });
 
   describe('encodeObject', () => {
     it('produces the correct value', () => {
-      const input = { test: 'bar', foo: '94', bar: '' , baz: '-baz-'};
+      const input = { test: 'bar', foo: '94', bar: '', baz: '-baz-' };
       const expectedOutput = 'test-bar_foo-94_bar-_baz--baz-';
       expect(encodeObject(input, '-', '_')).toBe(expectedOutput);
-      expect(encodeObject(undefined)).not.toBeDefined();
-      expect(encodeObject({})).not.toBeDefined();
+      expect(encodeObject(undefined)).toBeUndefined();
+      expect(encodeObject(null)).toBeNull();
+      expect(encodeObject({})).toBe('');
     });
   });
 
   describe('decodeObject', () => {
     it('produces the correct value', () => {
-      const output = decodeObject('foo-bar_jim-grill_iros-91_baz-_t--x-');
+      const output = decodeObject('foo-bar_jim-grill_kly-91_baz-_t--x-');
       const expectedOutput = {
         foo: 'bar',
         jim: 'grill',
-        iros: '91',
+        kly: '91',
         baz: '',
-        t: '-x-'
+        t: '-x-',
       };
       expect(output).toEqual(expectedOutput);
-      expect(decodeObject(undefined)).not.toBeDefined();
-      expect(decodeObject('')).not.toBeDefined();
+      expect(decodeObject(undefined)).toBeUndefined();
+      expect(decodeObject('')).toEqual({});
+      expect(decodeObject(null)).toBeNull();
     });
 
     it('handles malformed input', () => {
@@ -263,99 +347,44 @@ describe('serialize', () => {
     });
   });
 
-  describe('encodeDelimitedArray', () => {
-    it('produces the correct value', () => {
-      const input = ['a', 'b', 'c'];
-      expect(encodeDelimitedArray(input)).toBe('a_b_c');
-      expect(encodeDelimitedArray(undefined)).not.toBeDefined();
-    });
-  });
-
-  describe('decodeDelimitedArray', () => {
-    it('produces the correct value', () => {
-      const output = decodeDelimitedArray('a_b_c');
-      const expectedOutput = ['a', 'b', 'c'];
-
-      expect(output).toEqual(expectedOutput);
-      expect(decodeDelimitedArray(undefined)).not.toBeDefined();
-      expect(decodeDelimitedArray('')).not.toBeDefined();
-    });
-
-    it('filters empty values', () => {
-      expect(decodeDelimitedArray('__')).toEqual([]);
-    });
-
-    it('handles array of values', () => {
-      expect(decodeDelimitedArray(['foo_bar', 'jim_grill'])).toEqual([
-        'foo',
-        'bar',
-      ]);
-    });
-  });
-
-  describe('encodeDelimitedNumericArray', () => {
-    it('produces the correct value', () => {
-      const input = [9, 4, 0];
-      expect(encodeDelimitedNumericArray(input)).toBe('9_4_0');
-      expect(encodeDelimitedNumericArray(undefined)).not.toBeDefined();
-    });
-  });
-
-  describe('decodeDelimitedNumericArray', () => {
-    it('produces the correct value', () => {
-      const output = decodeDelimitedNumericArray('9_4_0');
-      const expectedOutput = [9, 4, 0];
-
-      expect(output).toEqual(expectedOutput);
-      expect(decodeDelimitedNumericArray(undefined)).not.toBeDefined();
-      expect(decodeDelimitedNumericArray('')).not.toBeDefined();
-    });
-
-    it('filters empty values', () => {
-      expect(decodeDelimitedNumericArray('__')).toEqual([]);
-    });
-
-    it('handles array of values', () => {
-      expect(decodeDelimitedNumericArray(['1_2', '3_4'])).toEqual([1, 2]);
-    });
-  });
-
   describe('encodeNumericObject', () => {
     it('produces the correct value', () => {
       const input = { test: 55, foo: 94 };
       const expectedOutput = 'test-55_foo-94';
       expect(encodeNumericObject(input, '-', '_')).toBe(expectedOutput);
-      expect(encodeNumericObject(undefined)).not.toBeDefined();
-      expect(encodeNumericObject({})).not.toBeDefined();
+      expect(encodeNumericObject(undefined)).toBeUndefined();
+      expect(encodeNumericObject(null)).toBeNull();
+      expect(encodeNumericObject({})).toBe('');
     });
   });
 
   describe('decodeNumericObject', () => {
     it('produces the correct value', () => {
-      const output = decodeNumericObject('foo-55_jim-100_iros-94');
+      const output = decodeNumericObject('foo-55_jim-100_kly-94');
       const expectedOutput = {
         foo: 55,
         jim: 100,
-        iros: 94,
+        kly: 94,
       };
       expect(output).toEqual(expectedOutput);
-      expect(decodeNumericObject(undefined)).not.toBeDefined();
-      expect(decodeNumericObject('')).not.toBeDefined();
+      expect(decodeNumericObject(undefined)).toBeUndefined();
+      expect(decodeNumericObject(null)).toBeNull();
+      expect(decodeNumericObject('')).toEqual({});
     });
 
     it('produces the correct value for negative numbers', () => {
-      const output = decodeNumericObject('foo--55_jim--100_iros--94');
+      const output = decodeNumericObject('foo--55_jim--100_kly--94');
       const expectedOutput = {
         foo: -55,
         jim: -100,
-        iros: -94,
+        kly: -94,
       };
       expect(output).toEqual(expectedOutput);
     });
 
     it('handles malformed input', () => {
       expect(decodeNumericObject('foo-bar-jim-grill')).toEqual({
-        foo: undefined,
+        foo: NaN,
       });
       expect(decodeNumericObject('foo_bar_jim_grill')).toEqual({
         foo: undefined,
